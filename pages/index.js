4229565c16a6bb82b9949707907eb33a770dc02b
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-08-30 16:47:07 -1000

    lesson 10: use local data with pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home ({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +23,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className="text-xl p-xl">
+        <h2 className="text-3xl">Blog</h2>
+        <ul className="list">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="list-item" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
